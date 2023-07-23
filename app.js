a9baa957a7cd3631455c449a204332b24f23c058
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-07-23 17:47:12 +0530

    done with js

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 let tbtn = document.querySelector("#tbtn");
-let inputvalue = document.querySelector("#input-area").value;
+var inputvalue = document.querySelector("#input-area")
+var outputvalue = document.querySelector("#output-area")
 
-tbtn.addEventListener("click",display);
+var newUrl;
+var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" //test server
+//var serverUrl = "https://api.funtranslations.com/translate/pirate.json"   //main server
+
+function constructUrl(text) {
+    return serverUrl + "?text=" + text; 
+}
+
+function translation(url){
+    fetch(url)
+    .then(function responsehandler(response){
+        return response.json();
+    })
+    .then(function consolelog(json){
+        outputvalue.innerText = json.contents.translated;   //output
+    })
+    .catch(errorhandler);
+}
+
+function errorhandler(error) {
+    console.log("something wrong with the server ",error);
+    alert("server did not respond !");
+}
 
 function display() {
-    console.log("Button clicked!");
-    let inputvalue = document.querySelector("#input-area").value;
-    console.log(inputvalue);
-    document.querySelector("#output-area").innerText = inputvalue;
+    newUrl = constructUrl(inputvalue.value);  //input
+    var translatedText = translation(newUrl);
 }
+
+tbtn.addEventListener("click",display);
